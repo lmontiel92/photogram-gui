64c5bbd1563572180db0289fa47479b5b3f56bb0
lmontiel92 <146214091+lmontiel92@users.noreply.github.com>
2023-11-23 04:29:39 +0000

    adding photo show page

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -2,10 +2,20 @@ class PhotosController < ApplicationController
 
   def index
     
-    @list_of_photos = Photo.all
+    @list_of_photos = Photo.all.order({:created_at => :desc})
 
     render({:template => "photo_templates/index"})
 
   end
+
+  def show
+
+    photo_id = params.fetch("path_id")
+  
+    @the_photo = Photo.where({:id => photo_id}).at(0)
+  
+    render({:template => "photo_templates/show"})    
+  
+    end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   #Users routes
  
+  get("/", {:controller=> "users",:action=> "index" })
+  
   get("/users", {:controller=> "users",:action=> "index" })
 
   get("/users/:path_username", {:controller=> "users",:action=> "show" })
@@ -9,5 +11,7 @@ Rails.application.routes.draw do
   #Photos routes
 
   get("/photos", {:controller=> "photos",:action=> "index" })
+
+  get("/photos/:path_id", {:controller=> "photos",:action=> "show" })
 
 end
